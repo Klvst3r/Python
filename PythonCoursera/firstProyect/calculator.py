5b7278e8f7f6dce65c5b9df2ef07e407a5afbf92
Klvst3r <klvst3r@gmail.com>
2018-10-02 12:22:36 -0500

    Branch First Proyect

---
+++ PythonCoursera/firstProyect/calculator.py
@@ -0,0 +1,23 @@
+python
+2 + 2
+123*123
+
+Variables
+x = 2
+y = 2
+x * y
+x = 4
+x + y
+x * y
+
+Imagina que asigno a la variable x el valor 0 (x=0) y a la variable y el valor 2 (y=2). Luego asigno a la variable res la suma de x más y (res=x+y). Luego cambio el valor de x asignándole el valor 6 y repito la suma anterior (res=x+y). ¿Cuánto dará el valor de la variable res al final?
+
+>>> x = 0
+>>> y = 2
+>>> res = x + y
+>>> x = 6
+>>> res = x + y
+>>> res
+8
+
+¡Excelente! El valor de la variable res es la suma del último valor que se le dio a la variable x (es decir, 6) más el valor que tenía la variable y (es decir, 2). La suma de 6+2 es igual a 8.
